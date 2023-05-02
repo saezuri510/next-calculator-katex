246db7b0d14a05216fc196fc58b5d4d9631217cc
saezuri510 <saezuru510@gmail.com>
2023-05-02 22:31:17 +0900

    feat: prismaを導入.

---
+++ prisma/seed.ts
@@ -0,0 +1,16 @@
+import { prisma } from "../src/lib/prisma";
+
+const main = async () => {
+  await prisma.user.createMany({
+    data: [{ name: "alice" }, { name: "bob" }],
+  });
+};
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
