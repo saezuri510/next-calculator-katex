a9ca7bd7665eaefe170765ef6bc11cb0a7d474ee
saezuri510 <saezuru510@gmail.com>
2023-05-02 21:37:21 +0900

    perf: 不要なライブラリを削除.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: ["./src/pages/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"],
   mode: "jit",
-  plugins: [require("tailwindcss-radix")()],
+  plugins: [],
   theme: {
     extend: {
       fontFamily: {
