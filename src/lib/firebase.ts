280aa9f7c6800316982113e314d525c98d403a0c
saezuri510 <saezuru510@gmail.com>
2023-05-03 23:14:00 +0900

    feat: firebase authの導入.

---
+++ src/lib/firebase.ts
@@ -0,0 +1,15 @@
+import { getApps, initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+};
+
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,9 +1,25 @@
 import { NextPage } from "next";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+import { Button } from "../components/ui/Button";
+import { auth } from "../lib/firebase";
 
 const AccountPage: NextPage = () => {
-  return (
+  const [user] = useAuthState(auth);
+
+  const handleClick = () => {
+    auth.signOut();
+    // TODO: useRouterを使ったページ遷移を実装する.
+    console.log("go to root");
+  };
+
+  return user ? (
     <div>
-      <div>Account</div>
+      <Button onClick={handleClick}>ログアウト</Button>
+    </div>
+  ) : (
+    <div>
+      <div>ログインされていません</div>
     </div>
   );
 };
