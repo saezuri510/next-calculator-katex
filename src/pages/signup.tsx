c2c80f18e43aa1f20bddb5af97fd4e7ff29630ec
saezuri510 <saezuru510@gmail.com>
2023-05-06 10:29:17 +0900

    feat: formでのログイン、新規登録機能の実装.

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,47 +1,41 @@
-import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { SubmitHandler, useForm } from "react-hook-form";
 
-import { auth, provider } from "../lib/firebase";
+import { auth } from "../lib/firebase";
 
 type FormValues = {
   email: string;
   password: string;
 };
 
-const LoginPage: NextPage = () => {
+const SignupPage: NextPage = () => {
   const router = useRouter();
 
   const { handleSubmit, register, reset } = useForm<FormValues>();
 
   const onSubmit: SubmitHandler<FormValues> = async (data) => {
-    await signInWithEmailAndPassword(auth, data.email, data.password);
+    await createUserWithEmailAndPassword(auth, data.email, data.password);
     reset();
-    router.back();
-  };
-
-  const handleClick = async () => {
-    await signInWithPopup(auth, provider);
     router.back();
   };
 
   return (
     <div>
-      <h1>ログイン</h1>
+      <h1>新規登録</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="email">メールアドレス</label>
         <input {...register("email")} id="email" />
         <label htmlFor="password">パスワード</label>
         <input {...register("password")} id="password" />
-        <button type="submit">ログイン</button>
+        <button type="submit">登録</button>
       </form>
-      <button onClick={handleClick}>googleでログイン</button>
-      <Link href="/signup">または新規登録</Link>
       <Link href="/">キャンセル</Link>
+      <Link href="/login">またはログイン</Link>
     </div>
   );
 };
 
-export default LoginPage;
+export default SignupPage;
