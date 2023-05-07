e5ff3afa61f230203b7c4e49f8c7d18cf4a9a9dc
saezuri510 <saezuru510@gmail.com>
2023-05-07 18:53:06 +0900

    refactor: prismaファイルを移動.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { prisma } from "../src/lib/prisma";
+import { prisma } from "./prisma";
 
 const main = async () => {
   await prisma.user.createMany({
--- src/graphql/context.ts
+++ src/graphql/context.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-import { prisma } from "../lib/prisma";
+import { prisma } from "../../prisma/prisma";
 
 export type Context = {
   prisma: PrismaClient;
