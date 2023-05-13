b7a32aa2d309dd5140fda65a8c2676cec26e2822
saezuri510 <saezuru510@gmail.com>
2023-05-13 15:05:07 +0900

    perf: modalライブラリをradixuiからheadlessuiに変更.

--- src/components/modals/SettingsModal.tsx
+++ src/components/modals/SettingsModal.tsx
@@ -1,5 +1,5 @@
-import * as Dialog from "@radix-ui/react-dialog";
-import { Dispatch, memo, ReactNode, SetStateAction } from "react";
+import { Dialog } from "@headlessui/react";
+import { Dispatch, memo, SetStateAction } from "react";
 import { ImCross } from "react-icons/im";
 
 import { auth } from "../../lib/firebase";
@@ -11,58 +11,57 @@ import { LinkButton } from "../ui/LinkButton";
 type Props = {
   isKeypadActive: boolean;
   setIsKeypadActive: Dispatch<SetStateAction<boolean>>;
-  children: ReactNode;
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
 };
 
 export const SettingsModal = memo(
-  ({ children, isKeypadActive, setIsKeypadActive }: Props): JSX.Element => {
+  ({ isKeypadActive, isOpen, setIsKeypadActive, setIsOpen }: Props): JSX.Element => {
     const { user } = useAuthContext();
 
+    const dialogClose = (func?: () => void) => {
+      setIsOpen(false);
+      if (func) {
+        func();
+      }
+    };
+
     return (
-      <Dialog.Root>
-        <Dialog.Trigger asChild>{children}</Dialog.Trigger>
-        <Dialog.Portal>
-          <Dialog.Overlay className="fixed inset-0 bg-black/50" />
-          <Dialog.Content className="fixed top-[50%] left-[50%] h-[80%] w-[90%] max-w-[350px] translate-x-[-50%] translate-y-[-50%] items-center justify-center rounded-[6px] bg-white p-[24px]">
-            <Dialog.Title className="text-[24px] font-bold">設定</Dialog.Title>
-            <div className="m-[12px] flex flex-col space-y-[8px]">
-              {!user && <LinkButton href="/login">ログイン、新規登録</LinkButton>}
-              <LinkButton href="/trend">今日の数学</LinkButton>
-              <LinkButton href="/share">シェア、エクスポート</LinkButton>
-              {user && (
-                <LinkButton href="/account">
-                  アカウント{auth.currentUser?.displayName}の設定
-                </LinkButton>
-              )}
-              <LinkButton href="/accessibility">アクセシビリティ、表示、言語</LinkButton>
-              <LinkButton href="/others">その他</LinkButton>
-              {isKeypadActive ? (
-                <Dialog.Close asChild>
-                  <Button color="blue" onClick={() => setIsKeypadActive(false)}>
-                    キーパットを非表示
-                  </Button>
-                </Dialog.Close>
-              ) : (
-                <Dialog.Close asChild>
-                  <Button color="blue" onClick={() => setIsKeypadActive(true)}>
-                    キーパッドを表示
-                  </Button>
-                </Dialog.Close>
-              )}
-              <Button color="blue" onClick={() => captureElement("#capture")}>
-                計算式を保存
+      <Dialog onClose={() => setIsOpen(false)} open={isOpen}>
+        <div className="fixed inset-0 bg-black/50" />
+        <Dialog.Panel className="fixed top-[50%] left-[50%] h-[80%] w-[90%] max-w-[350px] translate-x-[-50%] translate-y-[-50%] items-center justify-center rounded-[6px] bg-white p-[24px]">
+          <Dialog.Title className="text-[24px] font-bold">設定</Dialog.Title>
+          <div className="m-[12px] flex flex-col space-y-[8px]">
+            {!user && <LinkButton href="/login">ログイン、新規登録</LinkButton>}
+            <LinkButton href="/trend">今日の数学</LinkButton>
+            <LinkButton href="/share">シェア、エクスポート</LinkButton>
+            {user && (
+              <LinkButton href="/account">
+                アカウント{auth.currentUser?.displayName}の設定
+              </LinkButton>
+            )}
+            <LinkButton href="/accessibility">アクセシビリティ、表示、言語</LinkButton>
+            <LinkButton href="/others">その他</LinkButton>
+            {isKeypadActive ? (
+              <Button color="blue" onClick={() => dialogClose(() => setIsKeypadActive(false))}>
+                キーパットを非表示
               </Button>
-            </div>
-            <div className="absolute right-[24px] bottom-[24px]">
-              <Dialog.Close asChild>
-                <Button size="smallBox">
-                  <ImCross />
-                </Button>
-              </Dialog.Close>
-            </div>
-          </Dialog.Content>
-        </Dialog.Portal>
-      </Dialog.Root>
+            ) : (
+              <Button color="blue" onClick={() => dialogClose(() => setIsKeypadActive(true))}>
+                キーパッドを表示
+              </Button>
+            )}
+            <Button color="blue" onClick={() => dialogClose(() => captureElement("#capture"))}>
+              計算式を保存
+            </Button>
+          </div>
+          <div className="absolute right-[24px] bottom-[24px]">
+            <Button onClick={() => dialogClose()} size="smallBox">
+              <ImCross />
+            </Button>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
     );
   },
 );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,6 +24,7 @@ const IndexPage: NextPage = () => {
   const [calculationResults, setCalculationResults] = useState<string[]>([]);
   const [currentKeypad, setCurrentKeypad] = useState<KeypadCategory>("main");
   const [isKeypadActive, setIsKeypadActive] = useState<boolean>(true);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const { equation, equationControllers, setEquation } = useEquation("");
   const { ResponsiveSize } = useResponsiveSize();
@@ -58,113 +59,115 @@ const IndexPage: NextPage = () => {
   return (
     <div>
       <div>
-        <div>
-          {isMenuShown && (
-            <div>
-              <div className="fixed bottom-[64px] right-[64px]">
-                <SettingsModal
-                  isKeypadActive={isKeypadActive}
-                  setIsKeypadActive={setIsKeypadActive}
-                >
-                  <Button padding="regular" radius="circle" size="unspecified">
-                    <IoSettingsSharp className="h-[48px] w-[48px]" />
-                  </Button>
-                </SettingsModal>
-              </div>
-              <div className="fixed bottom-[120px] right-[56px]">
-                <Button color="gray" onClick={handleClick} padding="small" radius="circle">
-                  <ImCross />
-                </Button>
-              </div>
+        {isMenuShown && (
+          <div>
+            <div className="fixed bottom-[64px] right-[64px]">
+              <Button
+                onClick={() => setIsOpen(true)}
+                padding="regular"
+                radius="circle"
+                size="unspecified"
+              >
+                <IoSettingsSharp className="h-[48px] w-[48px]" />
+              </Button>
             </div>
-          )}
-          {/* TODO: テストコードなので削除する. */}
-          <ul>
-            {data?.users.map((user, idx) => {
-              return (
-                <li key={idx}>
-                  <p>{user?.name}</p>
-                </li>
-              );
-            })}
-          </ul>
-          <div id="capture">
-            {calculationResults.map((result, idx) => (
-              <div key={idx} className="flex">
-                <InlineMath>{String.raw`${result}`}</InlineMath>
-              </div>
-            ))}
+            <div className="fixed bottom-[120px] right-[56px]">
+              <Button color="gray" onClick={handleClick} padding="small" radius="circle">
+                <ImCross />
+              </Button>
+            </div>
           </div>
-        </div>
-        {isKeypadActive && (
-          <form className="fixed bottom-0 w-full bg-slate-300" onSubmit={handleSubmit}>
-            <input
-              className="h-[32px] w-full cursor-pointer rounded border border-red-500 font-roboto-medium font-medium"
-              onChange={(e) => setEquation(e.target.value)}
-              type="text"
-              value={equation}
-            />
-            <div className="h-[64px] w-full rounded border border-green-500 bg-white">
-              <InlineMath>{String.raw`${equation}`}</InlineMath>
-            </div>
-            <div className="flex">
-              <FuncKeypad {...equationControllers} currentKeypad={currentKeypad} />
-              <Func2Keypad {...equationControllers} currentKeypad={currentKeypad} />
-              <MainKeypad {...equationControllers} currentKeypad={currentKeypad} />
-            </div>
-            <div className="grid grid-cols-6 grid-rows-1">
-              {(() => {
-                if (ResponsiveSize === "sm") {
-                  switch (currentKeypad) {
-                    case "main":
-                      return (
-                        <Button onClick={() => setCurrentKeypad("func")}>
-                          <TbMathFunction />
-                        </Button>
-                      );
-                    case "func":
-                      return (
-                        <Button onClick={() => setCurrentKeypad("func2")}>
-                          <TbMathIntegral />
-                        </Button>
-                      );
-                    case "func2":
-                      return (
-                        <Button onClick={() => setCurrentKeypad("main")}>
-                          <TbMathSymbols />
-                        </Button>
-                      );
-                  }
-                } else {
-                  return currentKeypad !== "func2" ? (
-                    <Button onClick={() => setCurrentKeypad("func2")}>
-                      <TbMathIntegral />
-                    </Button>
-                  ) : (
-                    <Button onClick={() => setCurrentKeypad("func")}>
-                      <TbMathFunction />
-                    </Button>
-                  );
-                }
-              })()}
-
-              <Button onClick={() => setCalculationResults([])}>
-                <FaEraser />
-              </Button>
-              <SettingsModal isKeypadActive={isKeypadActive} setIsKeypadActive={setIsKeypadActive}>
-                <Button>
-                  <IoSettingsSharp />
-                </Button>
-              </SettingsModal>
-              <Button onClick={() => setEquation((prev) => prev.slice(0, -1))}>DEL</Button>
-              <Button onClick={equationControllers.reset}>AC</Button>
-              <Button color="blue" type="submit">
-                <IoArrowRedoSharp />
-              </Button>
-            </div>
-          </form>
         )}
+        {/* TODO: テストコードなので削除する. */}
+        <ul>
+          {data?.users.map((user, idx) => {
+            return (
+              <li key={idx}>
+                <p>{user?.name}</p>
+              </li>
+            );
+          })}
+        </ul>
+        <div id="capture">
+          {calculationResults.map((result, idx) => (
+            <div key={idx} className="flex">
+              <InlineMath>{String.raw`${result}`}</InlineMath>
+            </div>
+          ))}
+        </div>
       </div>
+      {isKeypadActive && (
+        <form className="fixed bottom-0 w-full bg-slate-300" onSubmit={handleSubmit}>
+          <input
+            className="h-[32px] w-full cursor-pointer rounded border border-red-500 font-roboto-medium font-medium"
+            onChange={(e) => setEquation(e.target.value)}
+            type="text"
+            value={equation}
+          />
+          <div className="h-[64px] w-full rounded border border-green-500 bg-white">
+            <InlineMath>{String.raw`${equation}`}</InlineMath>
+          </div>
+          <div className="flex">
+            <FuncKeypad {...equationControllers} currentKeypad={currentKeypad} />
+            <Func2Keypad {...equationControllers} currentKeypad={currentKeypad} />
+            <MainKeypad {...equationControllers} currentKeypad={currentKeypad} />
+          </div>
+          <div className="grid grid-cols-6 grid-rows-1">
+            {(() => {
+              if (ResponsiveSize === "sm") {
+                switch (currentKeypad) {
+                  case "main":
+                    return (
+                      <Button onClick={() => setCurrentKeypad("func")}>
+                        <TbMathFunction />
+                      </Button>
+                    );
+                  case "func":
+                    return (
+                      <Button onClick={() => setCurrentKeypad("func2")}>
+                        <TbMathIntegral />
+                      </Button>
+                    );
+                  case "func2":
+                    return (
+                      <Button onClick={() => setCurrentKeypad("main")}>
+                        <TbMathSymbols />
+                      </Button>
+                    );
+                }
+              } else {
+                return currentKeypad !== "func2" ? (
+                  <Button onClick={() => setCurrentKeypad("func2")}>
+                    <TbMathIntegral />
+                  </Button>
+                ) : (
+                  <Button onClick={() => setCurrentKeypad("func")}>
+                    <TbMathFunction />
+                  </Button>
+                );
+              }
+            })()}
+
+            <Button onClick={() => setCalculationResults([])}>
+              <FaEraser />
+            </Button>
+            <Button onClick={() => setIsOpen(true)}>
+              <IoSettingsSharp />
+            </Button>
+            <Button onClick={() => setEquation((prev) => prev.slice(0, -1))}>DEL</Button>
+            <Button onClick={equationControllers.reset}>AC</Button>
+            <Button color="blue" type="submit">
+              <IoArrowRedoSharp />
+            </Button>
+          </div>
+        </form>
+      )}
+      <SettingsModal
+        isKeypadActive={isKeypadActive}
+        isOpen={isOpen}
+        setIsKeypadActive={setIsKeypadActive}
+        setIsOpen={setIsOpen}
+      />
     </div>
   );
 };
