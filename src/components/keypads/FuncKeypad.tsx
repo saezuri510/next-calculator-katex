84324e0eb430e30bc5872a360a4b8a3b33e9ad09
saezuri510 <saezuru510@gmail.com>
2023-05-16 17:14:40 +0900

    refactor: htmlタグ内の文字列の扱い方を統一.

--- src/components/keypads/FuncKeypad.tsx
+++ src/components/keypads/FuncKeypad.tsx
@@ -17,23 +17,23 @@ export const FuncKeypad = memo(({ currentKeypad, entrySymbol }: KeypadProps): JS
     >
       <Button onClick={() => entrySymbol("<")}>{"<"}</Button>
       <Button onClick={() => entrySymbol("\\leqq")}>
-        <InlineMath>{"\\leqq"}</InlineMath>
+        <InlineMath>\leqq</InlineMath>
       </Button>
       <Button onClick={() => entrySymbol(">")}>{">"}</Button>
       <Button onClick={() => entrySymbol("\\geqq")}>
-        <InlineMath>{"\\geqq"}</InlineMath>
+        <InlineMath>\geqq</InlineMath>
       </Button>
       <Button onClick={() => entrySymbol("\\log_2{3}")}>
         <InlineMath>{"\\log_2{3}"}</InlineMath>
       </Button>
       <Button onClick={() => entrySymbol("\\pi")}>
-        <InlineMath>{"\\pi"}</InlineMath>
+        <InlineMath>\pi</InlineMath>
       </Button>
       <Button onClick={() => entrySymbol("\\theta")}>
-        <InlineMath>{"\\theta"}</InlineMath>
+        <InlineMath>\theta</InlineMath>
       </Button>
       <Button onClick={() => entrySymbol("30^\\circ")}>
-        <InlineMath>{"30^\\circ"}</InlineMath>
+        <InlineMath>30^\circ</InlineMath>
       </Button>
       <Button onClick={() => entrySymbol("⌊⌋")}>⌊⌋</Button>
       <Button onClick={() => entrySymbol("\\displaystyle\\sum_{i=1}^n{3}")}>
@@ -58,26 +58,26 @@ export const FuncKeypad = memo(({ currentKeypad, entrySymbol }: KeypadProps): JS
         <InlineMath>{"{}_1 C_{2}"}</InlineMath>
       </Button>
       <Button onClick={() => entrySymbol("\\Harr")}>
-        <InlineMath>{"\\Harr"}</InlineMath>
+        <InlineMath>\Harr</InlineMath>
       </Button>
       <Button onClick={() => entrySymbol("\\sqrt[2]{3}")}>
         <InlineMath>{"\\sqrt[2]{3}"}</InlineMath>
       </Button>
       <Button onClick={() => entrySymbol("≒")}>≒</Button>
       <Button onClick={() => entrySymbol("\\pm")}>
-        <InlineMath>{"\\pm"}</InlineMath>
+        <InlineMath>\pm</InlineMath>
       </Button>
       <Button onClick={() => entrySymbol("\\neq")}>
-        <InlineMath>{"\\neq"}</InlineMath>
+        <InlineMath>\neq</InlineMath>
       </Button>
       <Button onClick={() => entrySymbol("\\overline{3}")}>
         <InlineMath>{"\\overline{3}"}</InlineMath>
       </Button>
       <Button onClick={() => entrySymbol("\\therefore")}>
-        <InlineMath>{"\\therefore"}</InlineMath>
+        <InlineMath>\therefore</InlineMath>
       </Button>
       <Button onClick={() => entrySymbol("\\equiv")}>
-        <InlineMath>{"\\equiv"}</InlineMath>
+        <InlineMath>\equiv</InlineMath>
       </Button>
     </div>
   );
