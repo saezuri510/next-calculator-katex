6b6f4728da144bd57816af733043d52140f10e3c
saezuri510 <saezuru510@gmail.com>
2023-05-16 17:07:55 +0900

    feat: keypadの記号の追加.

--- src/components/keypads/Func2Keypad.tsx
+++ src/components/keypads/Func2Keypad.tsx
@@ -14,7 +14,76 @@ export const Func2Keypad = memo(({ currentKeypad, entrySymbol }: KeypadProps): J
       )}
     >
       <Button onClick={() => entrySymbol("\\in")}>
-        <InlineMath>{"\\in"}</InlineMath>
+        <InlineMath>\in</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\notin")}>
+        <InlineMath>\notin</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\subset")}>
+        <InlineMath>\subset</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\ni")}>
+        <InlineMath>\ni</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\notni")}>
+        <InlineMath>\notni</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\supset")}>
+        <InlineMath>\supset</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\cap")}>
+        <InlineMath>\cap</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\cup")}>
+        <InlineMath>\cup</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\varnothing")}>
+        <InlineMath>\varnothing</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\because")}>
+        <InlineMath>\because</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\iff")}>
+        <InlineMath>\iff</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\square")}>
+        <InlineMath>\square</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\triangle")}>
+        <InlineMath>\triangle</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\angle")}>
+        <InlineMath>\angle</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\gets")}>
+        <InlineMath>\gets</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\to")}>
+        <InlineMath>\to</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\implies")}>
+        <InlineMath>\implies</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\bot")}>
+        <InlineMath>\bot</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\overline{AB}")}>
+        <InlineMath>{"\\overline{AB}"}</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\blacksquare")}>
+        <InlineMath>\blacksquare</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\N")}>
+        <InlineMath>\N</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\Z")}>
+        <InlineMath>\Z</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\R")}>
+        <InlineMath>\R</InlineMath>
+      </Button>
+      <Button onClick={() => entrySymbol("\\overrightarrow{A}")}>
+        <InlineMath>{"\\overrightarrow{A}"}</InlineMath>
       </Button>
     </div>
   );
